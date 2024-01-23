ee1a9651636ceb8d4371dc48a874cdeeab40d755
bomeekim <thespringg@naver.com>
2024-01-23 17:36:17 +0900

    feature: VirtuosoGrid 적용 (렌더링까지 완료, 스크롤 이슈 확인 필요)

--- src/components/products/Products.tsx
+++ src/components/products/Products.tsx
@@ -1,31 +1,72 @@
-import { Grid } from '@mui/material'
+import { Grid, Skeleton, GridProps } from '@mui/material'
 import productsData from '../../data/products.json'
 import { IProductItem } from '../../data/products/IProduct'
 import ProductCard from './ProductCard'
+import { useMemo } from 'react'
+import { VirtuosoGrid } from 'react-virtuoso'
+import styled from '@emotion/styled'
 
 export default function Products() {
-  const productDataItemList = productsData.data.item_list as unknown as IProductItem[]
+  const productDataItemList = useMemo(
+    () => productsData.data.item_list as unknown as IProductItem[],
+    [],
+  )
+
+  const ItemContainer = styled.div`
+    display: flex;
+    flex: none;
+  `
+
+  const ListContainer = styled.div`
+    padding: 0px 16px 32px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 16px 8px;
+  `
+
+  const ItemWrapper = styled.div`
+    flex: 1;
+  `
 
   return (
-    <Grid container spacing={2} sx={{ p: 2 }}>
-      {productDataItemList.map(({ shop, product }, index) => (
-        <Grid item xs={4} key={index}>
-          <ProductCard
-            key={index}
-            imageUrl={product.webp_image_url}
-            sellerName={shop.name}
-            productName={product.title}
-            originPrice={product.max_price_with_currency.price_without_decimal}
-            discountRate={product.discount_rate}
-            discountTitle={product.discount_info?.title}
-            discountColor={product.discount_info?.color}
-            finalPrice={product.final_price}
-            badgeList={product.badge_list}
-            reviewScore={product.review_score}
-            reviewCount={product.review_count}
-          />
-        </Grid>
-      ))}
-    </Grid>
+    <VirtuosoGrid
+      style={{ height: '1000px' }}
+      totalCount={19}
+      overscan={3}
+      components={{
+        Item: ItemContainer,
+        // TODO ts error
+        // @ts-ignore
+        List: ListContainer,
+        ScrollSeekPlaceholder: () => <Skeleton variant="rectangular" />,
+      }}
+      itemContent={(index) => {
+        const productItem = productDataItemList[index]
+        const { shop, product } = productItem
+        return (
+          <ItemWrapper>
+            <ProductCard
+              key={index}
+              imageUrl={product.webp_image_url}
+              sellerName={shop.name}
+              productName={product.title}
+              originPrice={product.max_price_with_currency.price_without_decimal}
+              discountRate={product.discount_rate}
+              discountTitle={product.discount_info?.title}
+              discountColor={product.discount_info?.color}
+              finalPrice={product.final_price}
+              badgeList={product.badge_list}
+              reviewScore={product.review_score}
+              reviewCount={product.review_count}
+            />
+          </ItemWrapper>
+        )
+      }}
+      scrollSeekConfiguration={{
+        enter: (velocity) => Math.abs(velocity) > 200,
+        exit: (velocity) => Math.abs(velocity) < 30,
+        change: (_, range) => console.log({ range }),
+      }}
+    />
   )
 }
